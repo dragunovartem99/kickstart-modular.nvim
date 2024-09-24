1d29c3d1b4c8a12d4216205f7ef5f624932d03d8
Artem Dragunov <dragunovartem99@gmail.com>
2024-09-24 20:36:27 +0300

    Change default indentation

--- lua/options.lua
+++ lua/options.lua
@@ -53,6 +53,9 @@ vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
+-- Sets tab width (indentation)
+vim.opt.tabstop = 4
+
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
