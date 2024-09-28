55688ef8935b52eb39902223ba8777655ea80a09
Artem Dragunov <dragunovartem99@gmail.com>
2024-09-29 00:37:34 +0300

    Add more frontend LSPs

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -209,6 +209,8 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'html-lsp',
+        'css-lsp',
         'typescript-language-server',
         'prettierd',
       })
