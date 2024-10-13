e34152c9a297d05aaf909de8baa1822ab6a40b18
Artem Dragunov <dragunovartem99@gmail.com>
2024-10-13 16:24:39 +0300

    Set softtabstop

--- lua/options.lua
+++ lua/options.lua
@@ -55,6 +55,7 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Sets tab width (indentation)
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
