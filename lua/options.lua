675dc95b0c9286f5cf5008c2e93179e64c988bf9
Artem Dragunov <dragunovartem99@gmail.com>
2024-10-13 16:29:42 +0300

    Revert "Set softtabstop"
    This reverts commit e34152c9a297d05aaf909de8baa1822ab6a40b18.

--- lua/options.lua
+++ lua/options.lua
@@ -55,7 +55,6 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Sets tab width (indentation)
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
