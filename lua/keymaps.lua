a1c6c31b07a7109be9e2d3f53565c1727c4fdf75
Artem Dragunov <dragunovartem99@gmail.com>
2024-11-29 22:46:46 +0300

    Ensure cursor stays centered after scrolling or searching

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -22,6 +22,12 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Ensure cursor stays centered after scrolling or searching
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Center after scrolling down' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Center after scrolling up' })
+vim.keymap.set('n', 'n', 'nzz', { desc = 'Center after forward search' })
+vim.keymap.set('n', 'N', 'Nzz', { desc = 'Center after backward search' })
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
